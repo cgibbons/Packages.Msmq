b13083b89f243fa6a6e6e132d7aaf50c0ee3287a
Greg Bielleman <greg.bielleman@me.com>
2014-04-03 16:35:24 +1100

    Stop popup when elevated

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,13 +1,22 @@
 $packageName = 'NServiceBus.MSMQ'
-$tempFile = [System.IO.Path]::GetTempFileName()
+
 $psFile = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 'setup.ps1'
-try
-{
-	Start-ChocolateyProcessAsAdmin "& `'$psFile`' `'$tempFile`'"
-	Get-Content $tempFile
-}
-catch
-{
-	Get-Content $tempFile
-	throw $_
+$identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal($identity)
+
+if ($principal.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+	& "$psFile"
+} 
+else {
+	$tempFile = [System.IO.Path]::GetTempFileName()
+	try
+	{
+		Start-ChocolateyProcessAsAdmin "& `'$psFile`' `'$tempFile`'"
+		Get-Content $tempFile
+	}
+	catch
+	{
+		Get-Content $tempFile
+		throw $_
+	}
 }
